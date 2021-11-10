93e35fcf7cd4e460a1c3cc23561b7e9278d2a00e
SeanKuehl <teddscottfelt@gmail.com>
2021-11-10 08:44:27 -0500

    changed my-app to be a simple todo list app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,80 +2,120 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-	return (
-	<button className="square"
-	onClick={props.onClick}>
-		{props.value}
-	</button>
-	);
-}
 
-class Board extends React.Component {
+
+
+
+
+class GetInformation extends React.Component {
+	
+  
 	
   constructor(props) {
 	  super(props);
+	  
 	  this.state = {
-		  squares: Array(9).fill(null),
-		  xIsNext: true,
+		  name: "",
+		  date: "",
+		  eventItems: [],
+		  
 	  };
+	  
+	  //this.handleSubmit = this.handleSubmit.bind(this.state.name, this.state.date);
+	  this.handleSubmit = this.handleSubmit.bind(this);
+	  this.handleNameChange = this.handleNameChange.bind(this);
+	  this.handleDateChange = this.handleDateChange.bind(this);
+	  
+	  
   }
   
-  handleClick(i) {
-	  const squares = this.state.squares.slice();
-	  squares[i] = this.state.xIsNext ? 'X' : 'O';
-	  this.setState({squares: squares});
-  }
+  handleSubmit(event) {
 	
-  renderSquare(i) {
-    return (<Square value = {this.state.squares[i]} onClick={() => this.handleClick(i)}/>);
+	event.preventDefault();
+	
+	var newEntry = this.state.name +", "+ this.state.date;
+	const newItem = {
+      text: newEntry,
+      id: Date.now()
+    };
+	
+	
+	  
+	  //the entry is the name and date combined into one string
+	this.setState({eventItems: this.state.eventItems.concat(newItem)});
+	
+	
+	
+    
+    
   }
+  
+  handleNameChange(event) {
+		 
+		this.setState({name: event.target.value});
+		
+  }
+  
+  handleDateChange(event) {
+		 
+		this.setState({date: event.target.value});
+		
+  }
+  
+  
+  
+  
+  
 
   render() {
-    const status = 'Next player: X';
+    
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <div className="GetInformation"></div>
+        
+		
+		<form onSubmit={this.handleSubmit}>
+		<label for="name">Enter your name:</label><br></br>
+		<input type="text" id="name" name="enter_name"   onChange={this.handleNameChange} /><br></br>
+		<label for="date">Enter the date:</label><br></br>
+		<input type="text" id="date" name="enter_date"   onChange={this.handleDateChange} /><br></br>
+		<input type="submit" value = "Submit" /><br></br>
+		</form> 
+		<EventList items={this.state.eventItems}/>
       </div>
     );
   }
 }
 
-class Game extends React.Component {
+
+class EventList extends React.Component {
+	
+  constructor(props) {
+	  super(props);
+	  
+	  
+  }
+	
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     );
   }
 }
+
+
+
+
+
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <GetInformation />,
   document.getElementById('root')
 );
